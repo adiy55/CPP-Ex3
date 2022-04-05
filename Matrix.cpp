464e137fb4ba542a8e4d28e89f53499539a22a0f
adiy55 <adiyafe55@gmail.com>
2022-04-05 12:20:44 +0300

    Add missing bracket to Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -7,177 +7,178 @@ using std::vector;
 
 namespace zich {
 
-Matrix::Matrix(const std::vector<double> &matrix, int rows, int cols)
-        : _matrix(matrix), _rows(rows), _cols(cols) { checkInput(_matrix.size(), _rows, _cols); }
+    Matrix::Matrix(const std::vector<double> &matrix, int rows, int cols)
+            : _matrix(matrix), _rows(rows), _cols(cols) { checkInput(_matrix.size(), _rows, _cols); }
 
-Matrix::Matrix(std::vector<double> &&matrix, int rows, int cols) // rvalue reference // move constructor
-        : _matrix(std::move(matrix)), _rows(rows), _cols(cols) { checkInput(_matrix.size(), _rows, _cols); }
+    Matrix::Matrix(std::vector<double> &&matrix, int rows, int cols) // rvalue reference // move constructor
+            : _matrix(std::move(matrix)), _rows(rows), _cols(cols) { checkInput(_matrix.size(), _rows, _cols); }
 
-Matrix Matrix::operator-() const {
-    Matrix matrix{_matrix, _rows, _cols};
-    std::for_each(matrix._matrix.begin(), matrix._matrix.end(), [](double &val) {
-        if (val != 0) {
-            val = -val;
+    Matrix Matrix::operator-() const {
+        Matrix matrix{_matrix, _rows, _cols};
+        std::for_each(matrix._matrix.begin(), matrix._matrix.end(), [](double &val) {
+            if (val != 0) {
+                val = -val;
+            }
+        });
+        return matrix;
+    }
+
+    Matrix &Matrix::operator*=(const double scalar) {
+        std::for_each(_matrix.begin(), _matrix.end(), [scalar](double &val) { val *= scalar; });
+        return *this;
+    }
+
+    Matrix Matrix::operator+(const Matrix &other) const {
+        checkDimensionsEq(_rows, _cols, other._rows, other._cols);
+        Matrix res_matrix{_matrix, _rows, _cols};
+        for (uint i = 0; i < _matrix.size(); ++i) {
+            res_matrix._matrix[i] += other._matrix[i];
         }
-    });
-    return matrix;
-}
-
-Matrix &Matrix::operator*=(const double scalar) {
-    std::for_each(_matrix.begin(), _matrix.end(), [scalar](double &val) { val *= scalar; });
-    return *this;
-}
-
-Matrix Matrix::operator+(const Matrix &other) const {
-    checkDimensionsEq(_rows, _cols, other._rows, other._cols);
-    Matrix res_matrix{_matrix, _rows, _cols};
-    for (uint i = 0; i < _matrix.size(); ++i) {
-        res_matrix._matrix[i] += other._matrix[i];
+        return res_matrix;
     }
-    return res_matrix;
-}
 
-Matrix Matrix::operator-(const Matrix &other) const {
-    checkDimensionsEq(_rows, _cols, other._rows, other._cols);
-    Matrix res_matrix{_matrix, _rows, _cols};
-    for (uint i = 0; i < res_matrix._matrix.size(); ++i) {
-        res_matrix._matrix[i] -= other._matrix[i];
-    }
-    return res_matrix;
-}
-
-Matrix Matrix::operator+() const {
-    return Matrix{{}, 0, 0};
-}
-
-Matrix &Matrix::operator+=(const Matrix &other) {
-    checkDimensionsEq(_rows, _cols, other._rows, other._cols);
-    for (uint i = 0; i < _matrix.size(); ++i) {
-        _matrix[i] += other._matrix[i];
-    }
-    return *this;
-}
-
-Matrix &Matrix::operator-=(const Matrix &other) {
-    checkDimensionsEq(_rows, _cols, other._rows, other._cols);
-
-    return *this;
-}
-
-bool Matrix::operator>(const Matrix &other) const {
-    return false;
-}
-
-bool Matrix::operator>=(const Matrix &other) const {
-    return false;
-}
-
-bool Matrix::operator<(const Matrix &other) const {
-    return false;
-}
-
-bool Matrix::operator<=(const Matrix &other) const {
-    return false;
-}
-
-bool Matrix::operator==(const Matrix &other) const {
-    checkDimensionsEq(_rows, _cols, other._rows, other._cols);
-    for (uint i = 0; i < _matrix.size(); ++i) {
-        if (_matrix[i] != other._matrix[i]) {
-            return false;
+    Matrix Matrix::operator-(const Matrix &other) const {
+        checkDimensionsEq(_rows, _cols, other._rows, other._cols);
+        Matrix res_matrix{_matrix, _rows, _cols};
+        for (uint i = 0; i < res_matrix._matrix.size(); ++i) {
+            res_matrix._matrix[i] -= other._matrix[i];
         }
+        return res_matrix;
     }
-    return true;
-}
 
-bool Matrix::operator!=(const Matrix &other) const {
-    return !((*this) == other);
-}
+    Matrix Matrix::operator+() const {
+        return Matrix{{}, 0, 0};
+    }
+
+    Matrix &Matrix::operator+=(const Matrix &other) {
+        checkDimensionsEq(_rows, _cols, other._rows, other._cols);
+        for (uint i = 0; i < _matrix.size(); ++i) {
+            _matrix[i] += other._matrix[i];
+        }
+        return *this;
+    }
+
+    Matrix &Matrix::operator-=(const Matrix &other) {
+        checkDimensionsEq(_rows, _cols, other._rows, other._cols);
+
+        return *this;
+    }
+
+    bool Matrix::operator>(const Matrix &other) const {
+        return false;
+    }
+
+    bool Matrix::operator>=(const Matrix &other) const {
+        return false;
+    }
+
+    bool Matrix::operator<(const Matrix &other) const {
+        return false;
+    }
+
+    bool Matrix::operator<=(const Matrix &other) const {
+        return false;
+    }
+
+    bool Matrix::operator==(const Matrix &other) const {
+        checkDimensionsEq(_rows, _cols, other._rows, other._cols);
+        for (uint i = 0; i < _matrix.size(); ++i) {
+            if (_matrix[i] != other._matrix[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool Matrix::operator!=(const Matrix &other) const {
+        return !((*this) == other);
+    }
 
 // prefix (++i)
 
-Matrix &Matrix::operator++() {
-    std::for_each(_matrix.begin(), _matrix.end(), [](double &val) { val++; });
-    return *this;
-}
+    Matrix &Matrix::operator++() {
+        std::for_each(_matrix.begin(), _matrix.end(), [](double &val) { val++; });
+        return *this;
+    }
 
-Matrix &Matrix::operator--() {
-    std::for_each(_matrix.begin(), _matrix.end(), [](double &val) { val--; });
-    return *this;
-}
+    Matrix &Matrix::operator--() {
+        std::for_each(_matrix.begin(), _matrix.end(), [](double &val) { val--; });
+        return *this;
+    }
 
 // postfix (i++)
 
-Matrix Matrix::operator++(int) {
-    Matrix mat_copy{_matrix, _rows, _cols};
-    ++(*this);
-    return mat_copy;
-}
+    Matrix Matrix::operator++(int) {
+        Matrix mat_copy{_matrix, _rows, _cols};
+        ++(*this);
+        return mat_copy;
+    }
 
-Matrix Matrix::operator--(int) {
-    Matrix mat_copy{_matrix, _rows, _cols};
-    --(*this);
-    return mat_copy;
-}
+    Matrix Matrix::operator--(int) {
+        Matrix mat_copy{_matrix, _rows, _cols};
+        --(*this);
+        return mat_copy;
+    }
 
-Matrix Matrix::operator*(const Matrix &other) const {
-    return Matrix{{}, 0, 0};
-}
+    Matrix Matrix::operator*(const Matrix &other) const {
+        return Matrix{{}, 0, 0};
+    }
 
 // friend functions
 
-std::ostream &zich::operator<<(std::ostream &out, const Matrix &matrix) {
-    uint curr_row = 0;
-    uint curr_col = 0;
-    for (uint i = 0; i < matrix._rows; ++i) {
-        out << "[";
-        curr_row = i * static_cast<uint>(matrix._rows);
-        curr_col = curr_row + static_cast<uint>(matrix._cols);
-        for (uint j = curr_row; j < curr_col; ++j) {
-            out << matrix._matrix[j];
-            if (j < curr_col - 1) {
-                out << " ";
+    std::ostream &zich::operator<<(std::ostream &out, const Matrix &matrix) {
+        uint curr_row = 0;
+        uint curr_col = 0;
+        for (uint i = 0; i < matrix._rows; ++i) {
+            out << "[";
+            curr_row = i * static_cast<uint>(matrix._rows);
+            curr_col = curr_row + static_cast<uint>(matrix._cols);
+            for (uint j = curr_row; j < curr_col; ++j) {
+                out << matrix._matrix[j];
+                if (j < curr_col - 1) {
+                    out << " ";
+                }
             }
+            out << "]\n";
         }
-        out << "]\n";
+        return out;
     }
-    return out;
-}
 
-Matrix zich::operator*(const double scalar, const Matrix &matrix) {
-    Matrix res_matrix{matrix._matrix, matrix._rows, matrix._cols};
-    std::for_each(res_matrix._matrix.begin(), res_matrix._matrix.end(), [scalar](double &val) { val *= scalar; });
-    return res_matrix;
-}
+    Matrix zich::operator*(const double scalar, const Matrix &matrix) {
+        Matrix res_matrix{matrix._matrix, matrix._rows, matrix._cols};
+        std::for_each(res_matrix._matrix.begin(), res_matrix._matrix.end(), [scalar](double &val) { val *= scalar; });
+        return res_matrix;
+    }
 
-std::istream &zich::operator>>(std::istream &in, const Matrix &matrix) {
-    return in;
-}
+    std::istream &zich::operator>>(std::istream &in, const Matrix &matrix) {
+        return in;
+    }
 
 // class methods and helper functions
 
-void Matrix::checkInput(uint mat_size, int rows, int cols) {
-    if (rows < 0 || cols < 0 || mat_size != (rows * cols)) {
-        throw std::invalid_argument("Invalid matrix size!");
+    void Matrix::checkInput(uint mat_size, int rows, int cols) {
+        if (rows < 0 || cols < 0 || mat_size != (rows * cols)) {
+            throw std::invalid_argument("Invalid matrix size!");
+        }
     }
-}
 
-void Matrix::checkDimensionsMul(int mat1_cols, int mat2_rows) {
-    if (mat1_cols != mat2_rows) {
-        throw std::invalid_argument("Invalid dimensions for matrix multiplication!");
+    void Matrix::checkDimensionsMul(int mat1_cols, int mat2_rows) {
+        if (mat1_cols != mat2_rows) {
+            throw std::invalid_argument("Invalid dimensions for matrix multiplication!");
+        }
     }
-}
 
-void Matrix::checkDimensionsEq(int rows1, int cols1, int rows2, int cols2) {
-    if (rows1 != rows2 || cols1 != cols2) {
-        throw std::invalid_argument("Invalid dimensions for matrix addition or subtraction!");
+    void Matrix::checkDimensionsEq(int rows1, int cols1, int rows2, int cols2) {
+        if (rows1 != rows2 || cols1 != cols2) {
+            throw std::invalid_argument("Invalid dimensions for matrix addition or subtraction!");
+        }
     }
-}
 
-std::string Matrix::toString() const {
-    string mat_str;
-    std::for_each(_matrix.begin(), _matrix.end(),
-                  [&mat_str](const double &val) { mat_str += std::to_string(std::float_round_style(val)) + " "; });
-    return mat_str;
-}
+    std::string Matrix::toString() const {
+        string mat_str;
+        std::for_each(_matrix.begin(), _matrix.end(),
+                      [&mat_str](const double &val) { mat_str += std::to_string(std::float_round_style(val)) + " "; });
+        return mat_str;
+    }
 
+}
