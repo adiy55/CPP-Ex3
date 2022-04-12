8ec9fbc2ff346856cd6454ab473740de13978a68
adiy55 <adiyafe55@gmail.com>
2022-04-12 14:57:58 +0300

    Update main

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,26 @@
 #include "sources/Matrix.hpp"
+#include <limits>
 
 using namespace zich;
 using std::cin;
 using std::cout;
 using std::exception;
 
-void preset();
+void preset(const std::vector<double> &identity);
 
 void userInput(Matrix &);
 
-// todo: finish main
-// todo: add documentation
-// todo: cim
-
 int main() {
-//    std::cout << Matrix({{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 1});
-//    Matrix matrix{{1, 2, 3}, 3, 1};
-//    std::cout << "Enter Matrix values:\n";
-//    std::cin >> matrix;
-//    std::cout << "Output:\n" << matrix;
-    Matrix matrix{{1, 0, 0, 0, 1, 0, 0, 0, 1}, 3, 3};
+    const std::vector<double> identity{1, 0, 0, 0, 1, 0, 0, 0, 1};
+    Matrix matrix{identity, 3, 3};
     int mode;
     while (cin) {
         cout << "For preset input enter 1\nFor user input enter 2\nEnter any other character to exit\n";
         cin >> mode;
+        // https://www.tutorialspoint.com/what-is-the-use-of-cin-ignore-in-cplusplus
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //clear buffer before taking new line
         if (mode == 1) {
-            preset();
+            preset(identity);
         } else if (mode == 2) {
             userInput(matrix);
         } else {
@@ -36,10 +31,10 @@ int main() {
     return 0;
 }
 
-
-void preset() {
+void preset(const std::vector<double> &identity) {
     int selection;
-    cout << "Select:\n1 = 5x5 Identity Matrix | 2 = 4x4 Diagonal Matrix | 3 = 3x3 Symmetric Matrix\n";
+    cout << "Select:\n1 = 5x5 Identity Matrix | 2 = 4x4 Diagonal Matrix | 3 = 3x3 Symmetric Matrix | "
+            "4 = Matrix Multiplication Demo\n";
     cin >> selection;
     if (selection == 1) {
         cout << Matrix{{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1}, 5, 5} << '\n';
@@ -47,17 +42,20 @@ void preset() {
         cout << Matrix{{16, 0, 0, 0, 0, 30, 0, 0, 0, 0, 24, 0, 0, 0, 0, 23}, 4, 4} << '\n';
     } else if (selection == 3) {
         cout << Matrix{{1, 7, 3, 7, 4, 5, 3, 5, 0}, 3, 3} << '\n';
+    } else if (selection == 4) {
+        Matrix matrix1{identity, 3, 3};
+        Matrix matrix2{{1, 2, 3}, 3, 1};
+        cout << "Multiplying:\n" << matrix1 << "\nWith:\n" << matrix2 << "\nResult:\n" << matrix1 * matrix2 << '\n';
     } else {
         cout << "Invalid selection!\n";
     }
 }
 
-
 void userInput(Matrix &matrix) {
-    cout << "Enter (with spaces in between):\n";
+    cout << "Enter Matrix Values:\nValid Format Example: [1 0 0], [0 1 0], [0 0 1]\n";
     try {
         cin >> matrix;
-        cout << "Current Matrix:\n";
+        cout << "Updated Matrix:\n";
         cout << matrix << '\n';
     } catch (exception &ex) {
         cout << "Invalid input: " << ex.what() << '\n';
